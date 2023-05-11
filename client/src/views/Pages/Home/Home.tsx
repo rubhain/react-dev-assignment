5b4b3e503ac50cea7c6416ab015f0b8a876906c3
rubhain <bugrahantufekcioglu@gmail.com>
2023-05-11 21:12:14 +0300

    list added

--- client/src/views/Pages/Home/Home.tsx
+++ client/src/views/Pages/Home/Home.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { List } from "antd";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { DataType } from "../Companies/Companies";
+import { fetchCompanies } from "../../../storage/companiesSlice";
 
 function Home() {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCompanies());
+  }, [dispatch]);
+
+  const data: DataType[] = useSelector(
+    (state: any) => state.companies
+  ).companies;
+  const listSource = data.map((item: DataType) => item.companyName);
+
+  return (
+    <div>
+      {" "}
+      <List
+        size="large"
+        header={<h1>Latest Added Companies</h1>}
+        footer={
+          <div>{`There are "${listSource.length}" companies in the system.`}</div>
+        }
+        bordered
+        dataSource={listSource.reverse().slice(0, 3)}
+        renderItem={(item: any) => <List.Item>{item}</List.Item>}
+      />
+    </div>
+  );
 }
 
 export default Home;
